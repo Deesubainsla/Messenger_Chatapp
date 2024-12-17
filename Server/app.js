b826c199bd7129ad9dc0e1cd44981d3fbdc65b38
Deesubainsla <deesubainsla340@gmail.com>
2024-12-17 19:06:24 +0530

    adding url

--- Server/app.js
+++ Server/app.js
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:['http://localhost:5173','http://localhost:4173', process.env.CLIENT_URL]
+        origin:['http://localhost:5173','http://localhost:4173', 'https://messenger-chatapp-frontend.vercel.app']
     }//cors is important for socket connection
 });
 
